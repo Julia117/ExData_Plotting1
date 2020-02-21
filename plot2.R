f23080c70250011f44b2310732ff4c6cf0365005
Julia117 <julialysenko08@gmail.com>
2020-02-21 22:39:39 +0100

    add reading data

--- plot2.R
+++ plot2.R
@@ -1,3 +1,8 @@
+data <- data.table::fread(file = "household_power_consumption.txt", na.strings = "?")
+data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007"]
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+
 test <- group_by(data, data$DateTime)
 
 png(file = "plot2.png", width=480, height=480, bg="white")
--- plot4.R
+++ plot4.R
@@ -7,6 +7,11 @@ with(data, {
              ylab="Global Active Power (kilowatts)") 
 })
 
+data <- data.table::fread(file = "household_power_consumption.txt", na.strings = "?")
+data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007"]
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+
 with(data, {
         plot(DateTime, Voltage, type = "n", xlab = "datetime", 
              ylab="Voltage") 
