a034a69d21182e59368fbd13cc7e31b6aba3126e
Julia117 <julialysenko08@gmail.com>
2020-02-21 22:05:59 +0100

    add plot3

---
+++ plot3.R
@@ -0,0 +1,18 @@
+png(file = "plot3.png", width=480, height=480, bg="white")
+
+with(data, {
+        plot(DateTime, Sub_metering_1, ylim= c(0, 38), type = "n", xlab = "", 
+             ylab= "Energy sub metering", )
+        lines(DateTime, Sub_metering_1) 
+        lines(DateTime, Sub_metering_2, col="red") 
+        lines(DateTime, Sub_metering_3, col="blue") 
+        
+})
+
+legend(x="topright", legend=c("Sub_metering_1", "Sub_metering_2",
+                              "Sub_metering_3"), col=c("black", "red","blue"), 
+       lwd=1, lty=c(1,1,1), xjust=1, yjust=0, cex=1)
+dev.off()
+
+
+
