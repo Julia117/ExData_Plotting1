ae3df21ae1c0881b6b4e560c605fd29b45002712
Julia117 <julialysenko08@gmail.com>
2020-02-21 22:37:19 +0100

    fix plot2

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@ test <- group_by(data, data$DateTime)
 png(file = "plot2.png", width=480, height=480, bg="white")
 
 with(data, {
-        plot(DateTime, Global_active_power, pch = 20, type = "n", xlab = "", 
+        plot(DateTime, Global_active_power, type = "n", xlab = "", 
              ylab="Global Active Power (kilowatts)") 
         lines(DateTime, Global_active_power)
         })
